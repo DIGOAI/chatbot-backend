5aef55f103c998a49ab0965e93e538889e89fb3a
Juan Gahona <gahonajuanjo@gmail.com>
2023-08-25 16:25:10 -0500

    :memo: Add Ticket class docstring. - Added `Ticket` class docstring in the constructor.

--- src/models/ticket.py
+++ src/models/ticket.py
@@ -2,7 +2,16 @@ from typing import Optional
 
 
 class Ticket(object):
-    """Class to represent a ticket in SaragurosNet."""
+    """Class to represent a ticket in SaragurosNet.
+
+    Parameters:
+    contenido (str): The content of the ticket
+    asunto (str): The subject of the ticket
+    departamento (str): The department of the ticket
+    turno (str): The shift of the ticket
+    fechavisita (str): The date of the ticket
+    agendado (bool): If the ticket is scheduled or not
+    """
 
     def __init__(self, contenido: str, asunto: str, departamento: str, turno: str, fechavisita: str, agendado: bool):
         self.id: Optional[int] = None
