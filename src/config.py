2cbb621b37db08197923e692b58f9741774dede3
Juan Gahona <gahonajuanjo@gmail.com>
2023-08-25 13:57:26 -0500

    :art: :closed_lock_with_key: :wrench: Update config. - Replace atributes names from lowercase to uppercase. - Added a new way to check environment variables. - Change `SARAGUROS_TOKEN` to `SARAGUROS_API_TOKEN` env name.

--- src/config.py
+++ src/config.py
@@ -3,6 +3,7 @@ import os
 
 class Config:
     """ Config class to load environment variables. """
+
     _instance = None
 
     def __new__(cls):
@@ -13,35 +14,38 @@ class Config:
 
     def _load_config(self):
         """ Load environment variables. """
-        print("[INFO] Loading config...")
-        self.db_user = os.environ.get("DB_USER", "postgres")
-        self.db_password = os.environ.get("DB_PASSWORD")
-        self.db_host = os.environ.get("DB_HOST", "localhost")
-        self.db_port = os.environ.get("DB_PORT", "5432")
-        self.db_name = os.environ.get("DB_NAME", "postgres")
 
-        self.saraguros_api_url = os.environ.get("SARAGUROS_API_URL")
-        self.saraguros_token = os.environ.get("SARAGUROS_TOKEN")
+        print("[INFO] Loading config...")
+
+        self.DB_USER = os.environ.get("DB_USER", "postgres")
+        self.DB_PASSWORD = os.environ.get("DB_PASSWORD", "")
+        self.DB_HOST = os.environ.get("DB_HOST", "localhost")
+        self.DB_PORT = os.environ.get("DB_PORT", "5432")
+        self.DB_NAME = os.environ.get("DB_NAME", "postgres")
+
+        self.SARAGUROS_API_URL = os.environ.get("SARAGUROS_API_URL", "")
+        self.SARAGUROS_API_TOKEN = os.environ.get("SARAGUROS_API_TOKEN", "")
 
         self._verify()
 
     def _verify(self):
         """ Verify that all required environment variables are set. """
-        if not self.db_password:
+
+        if not self.DB_PASSWORD.strip():
             raise ValueError("DB_PASSWORD is not set")
-        if not self.saraguros_api_url:
+        if not self.SARAGUROS_API_URL.strip():
             raise ValueError("SARAGUROS_API_URL is not set")
-        if not self.saraguros_token:
-            raise ValueError("SARAGUROS_TOKEN is not set")
+        if not self.SARAGUROS_API_TOKEN.strip():
+            raise ValueError("SARAGUROS_API_TOKEN is not set")
 
 
 # Export a single instance of Config
 config = Config()
 
 if __name__ == "__main__":
-    print("DB_USER (config1):", config.db_user)
+    print("DB_USER (config1):", config.DB_USER)
 
     config2 = Config()
-    print("DB_USER (config2):", config2.db_user)
+    print("DB_USER (config2):", config2.DB_USER)
 
     print("config1 is config2:", config is config2)
