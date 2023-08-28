d162d78cc3241481f7a32da45439da2aa17c680d
Juan Gahona <gahonajuanjo@gmail.com>
2023-08-28 12:24:48 -0500

    :recycle: Marked progress on the old solution - Marked with the `=== COMPLETE ===` block comment, the helper functions refactored.

--- aws_solution.py
+++ aws_solution.py
@@ -1,16 +1,19 @@
 import json
-from twilio.rest import Client
 import os
 import re
-import psycopg2
-import requests
 import sys
 from datetime import datetime, timedelta
+
 import openai
+import psycopg2
+import requests
+from twilio.rest import Client
 
 ###########################################
 ## START - HELPER FUNCTIONS              ##
 ###########################################
+
+# === COMPLETE ===
 
 
 def ask_chatgpt(text):
@@ -20,7 +23,7 @@ def ask_chatgpt(text):
 
     completion = openai.Completion.create(
         model="text-davinci-003",
-        prompt=f'El sigiuente es un mensaje de un cliente en un canal de sorpote: {text}. A partir del mensaje del cliente determina a cuál de las siguientes categorías de atención pertenece el mensaje: Disponibilidad del servicio por falla técnica, Lentitud o baja velocidad del servicio, Problemas con la antena o baja señal de equipo CPE, Problemas del servicio por ausencia o falla en potencia óptica, Problemas con el router o señal WIFI, Cambio de clave WIFI, Bloqueo de servicios o accesos a internet, Falla masiva, Congelamiento temporal del servicio, Traslado del servicio, Cambio de condiciones del servicio, Suspensión Injustificada, Cesión de contrato, Servicios adicionales, Contratación de servicios, Equipos en comodato, Terminación de contrato, Certificaciones y paz y salvo, Fidelización, Reclamo sobre reporte a centrales de riesgos, Cambio de periodos de facturación, Reclamo sobre facturación, Descuento o compensación, Recurso de reposición, Recurso de reposición y en subsidio de apelación, Cumplimiento de una orden de la SIC, Sugerencias, Otras PQ. Selecciona además qué departamento debería atender al usuario: Soporte técnico, Ventas, Quejas y Sugerencias. El output debe ser estrictamente: categoría sugerida,departamento sugerido',
+        prompt=f'El siguiente es un mensaje de un cliente en un canal de sorpote: {text}. A partir del mensaje del cliente determina a cuál de las siguientes categorías de atención pertenece el mensaje: Disponibilidad del servicio por falla técnica, Lentitud o baja velocidad del servicio, Problemas con la antena o baja señal de equipo CPE, Problemas del servicio por ausencia o falla en potencia óptica, Problemas con el router o señal WIFI, Cambio de clave WIFI, Bloqueo de servicios o accesos a internet, Falla masiva, Congelamiento temporal del servicio, Traslado del servicio, Cambio de condiciones del servicio, Suspensión Injustificada, Cesión de contrato, Servicios adicionales, Contratación de servicios, Equipos en comodato, Terminación de contrato, Certificaciones y paz y salvo, Fidelización, Reclamo sobre reporte a centrales de riesgos, Cambio de periodos de facturación, Reclamo sobre facturación, Descuento o compensación, Recurso de reposición, Recurso de reposición y en subsidio de apelación, Cumplimiento de una orden de la SIC, Sugerencias, Otras PQ. Selecciona además qué departamento debería atender al usuario: Soporte técnico, Ventas, Quejas y Sugerencias. El output debe ser estrictamente: categoría sugerida,departamento sugerido',
         temperature=0.5,
         max_tokens=1024
     )
@@ -29,6 +32,10 @@ def ask_chatgpt(text):
     response = r.replace('[\s\.\n]+', '')
 
     return response
+# === COMPLETE ===
+
+
+# === COMPLETE ===
 
 
 def activar_servicio(usuario_id):
@@ -42,6 +49,9 @@ def activar_servicio(usuario_id):
 
     response = requests.post(url, headers=headers, data=json.dumps(data))
     return response.json()
+# === COMPLETE ===
+
+# === COMPLETE ===
 
 
 def create_ticket(contenido, asunto, departamento, turno, usuario_id, fechavisita, agendado):
@@ -61,6 +71,9 @@ def create_ticket(contenido, asunto, departamento, turno, usuario_id, fechavisit
 
     response = requests.post(url, headers=headers, data=json.dumps(data))
     return response.json()
+# === COMPLETE ===
+
+# === COMPLETE ===
 
 
 def parse_transaccion(image_text):
@@ -77,6 +90,9 @@ def parse_transaccion(image_text):
         monto = lines[15]
         oficina = lines[16]
         return nombre_usuario, fecha, monto, oficina
+# === COMPLETE ===
+
+# === COMPLETE ===
 
 
 def get_cedula_by_celular(celular):
@@ -119,6 +135,9 @@ def get_cedula_by_celular(celular):
             cur.close()
         if conn:
             conn.close()
+# === COMPLETE ===
+
+# === COMPLETE ===
 
 
 def getEstadoUsuario(celular):
@@ -172,6 +191,9 @@ def getEstadoUsuario(celular):
             cur.close()
         if conn:
             conn.close()
+# === COMPLETE ===
+
+# === COMPLETE ===
 
 
 def getUsuarioData(cedula):
@@ -183,6 +205,9 @@ def getUsuarioData(cedula):
     }
     response = requests.post(url, headers=headers, data=json.dumps(data))
     return response.json()
+# === COMPLETE ===
+
+# === COMPLETE ===
 
 
 def updateEstadoUsuario(cedula, celular, bot_estado, observaciones, actualizado, usuario_id):
@@ -229,6 +254,8 @@ def updateEstadoUsuario(cedula, celular, bot_estado, observaciones, actualizado,
             cur.close()
         if conn:
             conn.close()
+# === COMPLETE ===
+
 
 ###########################################
 ## END - HELPER FUNCTIONS                ##
