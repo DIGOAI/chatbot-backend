8b218e7fd2c35ade663f09d4ef26329281a31fd5
Juan Gahona <gahonajuanjo@gmail.com>
2023-08-30 15:04:43 -0500

    :zap: Barreled imports. - Barreled service imports.

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -1,1 +1,5 @@
+from src.services.chat_api import ChatApiService
+from src.services.chatgpt import ChatGPTService
+from src.services.ocr_lambda import OCRLambdaService
 from src.services.saragurosnet import SaragurosService
+from src.services.twilio import TwilioService
