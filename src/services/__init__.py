d001e2574783a2ba07d16bfffdcc98d8d6c67100
Juan Gahona <gahonajuanjo@gmail.com>
2023-09-05 14:20:42 -0500

    :art: :pencil2: Improve barrels. - Add service imports to `__all__` magic attribute.

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -3,3 +3,11 @@ from src.services.chatgpt import ChatGPTService
 from src.services.ocr_lambda import OCRLambdaService
 from src.services.saragurosnet import SaragurosService
 from src.services.twilio import TwilioService
+
+__all__ = [
+    'ChatApiService',
+    'ChatGPTService',
+    'OCRLambdaService',
+    'SaragurosService',
+    'TwilioService'
+]
