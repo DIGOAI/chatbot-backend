2d08722f7fee74b94d9db4d398398fa41d1d6788
Juan Gahona <gahonajuanjo@gmail.com>
2023-09-05 12:19:51 -0500

    :art: :zap: :sparkles: :construction: :recycle: - Added `condition` and `end` param to `Action` generic class. - `Action` generic class now use the `Logger` class. - The `context` of the `DecisionsTree` class can now be set. - Now the `DecisionsTree` class stores the `Actions` to build the tree. - Now the `DecisionsTree` class build the tree when it is executed. - Added the `ContextType` type class to manage the params. - Barrelled imports.

--- src/decisions_tree/action.py
+++ src/decisions_tree/action.py
@@ -1,6 +1,8 @@
-from typing import Callable, Generic, TypedDict, TypeVar
+from typing import Callable, Generic, Optional, TypedDict, TypeVar, cast
 
 from typing_extensions import Protocol
+
+from src.logger import Logger
 
 T = TypeVar('T', contravariant=True)
 
@@ -20,12 +22,21 @@ class Action(Generic[T]):
     func (Callable[[T], None]): The function to execute
     """
 
-    def __init__(self, id: str, func: ActionFunction[T]) -> None:
+    def __init__(self, id: str, func: ActionFunction[T], condition: Optional[bool] = None, end: bool = True) -> None:
         self.id = id
         self._func = func
+        self._condition = condition
+        self._end = end
 
-    def __call__(self, context: T) -> None:
-        self._func(context)
+    def __call__(self, context: T) -> bool:
+        if self._condition is None or self._condition:
+            Logger.info(
+                f"Executing action {self.id} - {cast(Callable[[T], None], self._func).__name__}")
+            self._func(context)
+            return not self._end
+        else:
+            Logger.info(f"Condition is False, skipping action {self.id}")
+            return True
 
 
 if __name__ == "__main__":
--- src/decisions_tree/decisions_tree.py
+++ src/decisions_tree/decisions_tree.py
@@ -1,6 +1,8 @@
-from typing import Any, Generic, TypeVar
+from functools import wraps
+from typing import Any, Callable, Generic, Optional, TypeVar, cast
 
-from src.decisions_tree.action import Action
+from src.decisions_tree.action import Action, ActionFunction
+from src.logger import Logger
 
 T = TypeVar('T')
 
@@ -14,18 +16,45 @@ class DecisionsTree(Generic[T]):
     context (T): The context to use in the actions
     """
 
-    def __init__(self, context: T) -> None:
+    def __init__(self, context: Optional[T] = None) -> None:
         self._tree: dict[str, Action[T]] = {}
-        self._context: T = context
+        self.context = context or cast(T, {})
+        self._functions: list[Callable[[], None]] = []
+
+    @property
+    def context(self) -> T:
+        return self.__context
+
+    @context.setter
+    def context(self, context: T) -> None:
+        self.__context = context
+
+    def setContext(self, context: T) -> None:
+        self.context = context
 
     def addAction(self, action: Action[T]) -> None:
         self._tree[action.id] = action
 
-    def __call__(self, status: str) -> Any:
-        # Verify if the status is in the tree
-        if status not in self._tree:
-            print(f"[ERROR][TREE]: Status '{status}' not found in the tree")
-            raise Exception(f"Status '{status}' not found in the tree")
+    def addActionDecorator(self, id: str, condition: Callable[[T], bool], end: bool = True):
+        def inner_decorator(func: ActionFunction[T]):
+            @wraps(func)
+            def wrapper() -> None:
+                self.addAction(
+                    Action(id, func, condition(self.context), end))
+            self._functions.append(wrapper)
+            return wrapper
+        return inner_decorator
 
-        # Execute the action
-        self._tree[status](self._context)
+    def __call__(self) -> Any:
+        Logger.info("Building the action tree")
+        for func in self._functions:
+            func()
+        Logger.info(f"Actions: {self._tree.keys()}")
+
+        Logger.info(f"Executing actions")
+
+        for _, action in self._tree.items():
+            if not action(self.context):
+                break
+
+        Logger.info(f"Finished executing actions")
