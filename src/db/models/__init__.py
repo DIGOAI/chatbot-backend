4c21d11e68f44c0fe0e9105abc6b105dd12aff13
Juan Gahona <gahonajuanjo@gmail.com>
2023-09-22 11:21:56 -0500

    :art::sparkles: Add messages migration. - Added `Message` db model class. - Generated `added_messages_table` migration.

--- src/db/models/__init__.py
+++ src/db/models/__init__.py
@@ -1,7 +1,11 @@
+from datetime import datetime
+from sqlalchemy import DateTime
+from sqlalchemy import ForeignKey, Integer, String, Text
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 __all__ = [
     'Base',
     'Client',
+    'Message',
 ]
 
 
@@ -37,8 +41,35 @@ class Client(Base):
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
     updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
 
+    messages: Mapped[list["Message"]] = relationship(back_populates="client")
 
     def __repr__(self) -> str:
         return f"<UserModel(id={self.id}, ci={self.ci}, name={self.names}, lastnames={self.lastnames}, phone={self.phone}, last_state={self.last_state}, saraguros_id={self.saraguros_id}, created_at={self.created_at}, updated_at={self.updated_at})>"
+
+
+class Message(Base):
+    """MessageModel class to handle the message model.
+
+    Attributes:
+    __tablename__ (str): The name of the table
+    id (int): The id of the message
+    message (str): The message of the message
+    client_id (int): The id of the client
+    created_at (datetime): The datetime when the message was created
+    updated_at (datetime): The datetime when the message was updated
+    """
+
+    __tablename__ = "messages"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    sender: Mapped[str] = mapped_column(String(13), nullable=False)
+    message: Mapped[str] = mapped_column(Text, nullable=False)
+    client_id: Mapped[int] = mapped_column(ForeignKey("clients.id"), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+
+    client: Mapped["Client"] = relationship(back_populates="messages")
+
+    def __repr__(self) -> str:
+        return f"<MessageModel(id={self.id}, sender={self.sender}, message={self.message}, client_id={self.client_id}, created_at={self.created_at})>"
 
 
