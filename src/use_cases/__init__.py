6ba2b86890dc5008bde863278dc0f2ea88027b8e
Juan Gahona <gahonajuanjo@gmail.com>
2023-09-28 13:41:08 -0500

    :sparkles: Added auth routes. - Implemented `UserRepository` class. - Implemented `create` repository method. - Implemented `get_user_by_email` repository method. - Implemented `RegisterNewUser` use case class. - Implemented `LoginUser` use case class. - Implemented `/auth/login` endpoint. - Implemented `/auth/register` endpoint. - Barrelled imports.

--- src/use_cases/__init__.py
+++ src/use_cases/__init__.py
@@ -1,6 +1,9 @@
+from src.use_cases.auth_use_cases import LoginUser, RegisterNewUser
 from src.use_cases.message_use_cases import get_last_messages, write_message
 
 __all__ = [
     "get_last_messages",
-    "write_message"
+    "write_message",
+    "RegisterNewUser",
+    "LoginUser",
 ]
