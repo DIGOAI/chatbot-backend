26d8cd15ea5249cfd43399e8998a43ef63464453
Juan Gahona <gahonajuanjo@gmail.com>
2023-10-02 15:06:19 -0500

    :sparkles: Add get client by phone route. - Added `get_client_by_phone` route.

--- src/routes/client_routes.py
+++ src/routes/client_routes.py
@@ -4,7 +4,7 @@ from fastapi import status as STATUS
 from src.middlewares import APITokenAuth, JWTBearer
 from src.middlewares.jwt_bearer import Role
 from src.models import Client, ClientInsert, GenericResponse
-from src.use_cases import GetClients, RegisterNewClient
+from src.use_cases import GetClientByPhone, GetClients, RegisterNewClient
 
 router = APIRouter(prefix="/client", tags=["Client"])
 
@@ -19,3 +19,9 @@ def get_clients():
 def add_client(client_insert: ClientInsert):
     register_new_client = RegisterNewClient()
     return register_new_client(client_insert)
+
+
+@router.get("/{client_phone}", response_model=GenericResponse[Client], dependencies=[Depends(APITokenAuth())])
+def get_client_by_phone(client_phone: str):
+    get_client_by_phone = GetClientByPhone()
+    return get_client_by_phone(client_phone)
