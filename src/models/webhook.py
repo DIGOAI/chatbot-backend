33fb9669d059ae8def52069c35c86f1114f336f1
Juan Gahona <gahonajuanjo@gmail.com>
2023-10-02 17:41:39 -0500

    :sparkles: Add Twilio hook event model. - Added `TwilioWebHook` event model. - Barrelled imports.

---
+++ src/models/webhook.py
@@ -0,0 +1,47 @@
+from typing import Annotated, Optional
+
+from fastapi import Form
+
+
+class TwilioWebHook():
+    """Model for Twilio's Event Webhook."""
+
+    def __init__(self, MessageSid: Annotated[str, Form(max_length=34, alias="MessageSid",
+                                                       description="Unique identifier for the message.")],
+                 SmsSid: Annotated[str, Form(max_length=34, alias="SmsSid",
+                                             description="Unique identifier for the SMS (Deprecated).")],
+                 AccountSid: Annotated[str, Form(max_length=34, alias="AccountSid",
+                                                 description="Unique identifier for the account.")],
+                 MessagingServiceSid: Annotated[str, Form(max_length=34, alias="MessagingServiceSid",
+                                                          description="Unique identifier for the messaging service.")],
+                 From: Annotated[str, Form(max_length=22, alias="From", description="The number that sent the message.")],
+                 To: Annotated[str, Form(max_length=22, alias="To", description="The number that received the message.")],
+                 Body: Annotated[str, Form(max_length=1600, alias="Body", description="The text body of the message.")],
+                 ProfileName: Annotated[str, Form(alias="ProfileName", description="The name of the WhatsApp profile.")],
+                 WaId: Annotated[str, Form(alias="WaId", description="The WhatsApp ID of the sender.")],
+                 NumMedia: Annotated[int, Form(alias="NumMedia",
+                                               description="The number of media items associated with the message.")] = 0,
+                 MediaContentType0: Annotated[Optional[str], Form(alias="MediaContentType0",
+                                                                  description="The content type of the media.")] = None,
+                 MediaUrl0: Annotated[Optional[str], Form(
+                     alias="MediaUrl0", description="The URL of the media.")] = None,
+                 Forwarded: Annotated[bool, Form(
+                     alias="Forwarded", description="Whether the message was forwarded.")] = False,
+                 FrequentlyForwarded: Annotated[bool, Form(alias="FrequentlyForwarded",
+                                                           description="Whether the message was frequently forwarded.")] = False,
+                 ButtonText: Annotated[Optional[str], Form(alias="ButtonText", description="The text of a Quick reply button.")] = None):
+        self.message_sid = MessageSid
+        self.sms_sid = SmsSid
+        self.account_sid = AccountSid
+        self.messaging_service_sid = MessagingServiceSid
+        self.from_number = From
+        self.to_number = To
+        self.body = Body
+        self.num_media = NumMedia
+        self.media_content_type = MediaContentType0
+        self.media_url = MediaUrl0
+        self.profile_name = ProfileName
+        self.wa_id = WaId
+        self.forwarded = Forwarded
+        self.frequently_forwarded = FrequentlyForwarded
+        self.button_text = ButtonText
