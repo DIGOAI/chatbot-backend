88ef6164c29db3705a8510cf901590ac685abbdd
Juan Gahona <gahonajuanjo@gmail.com>
2023-10-03 17:07:47 -0500

    :construction: Webhook response. - Now the webhook response to Twilio with message in `XML`

--- src/routes/twilio_routes.py
+++ src/routes/twilio_routes.py
@@ -1,6 +1,7 @@
+import json
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Response
 
 from src.models import TwilioWebHook
 
@@ -9,4 +10,14 @@ router = APIRouter(prefix="/twilio", tags=["Twilio"])
 
 @router.post("/hook")
 def twilio_hook(webhook: Annotated[TwilioWebHook, Depends()]):
-    return webhook
+    response_content = """<?xml version="1.0" encoding="UTF-8"?>
+    <Response>
+        <Message>
+        {message}
+        </Message>
+    </Response>
+    """
+
+    content = response_content.format(message=json.dumps(webhook.__dict__, indent=2))
+
+    return Response(content, media_type="application/xml")
