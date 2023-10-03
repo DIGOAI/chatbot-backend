b3b62946215cc0ec742c2746254195aa5f376e26
Juan Gahona <gahonajuanjo@gmail.com>
2023-10-03 12:10:02 -0500

    :zap: DB now use database url env. - Replace old URL construction with `DATABASSE_URL` env variable.

--- src/db/database.py
+++ src/db/database.py
@@ -1,36 +1,14 @@
-from typing import Any, TypedDict
+from typing import Any
 
 from psycopg import Cursor
 from sqlalchemy import Connection, ExecutionContext, create_engine
-from sqlalchemy.engine.url import URL
 from sqlalchemy.event import listens_for
 from sqlalchemy.orm.session import sessionmaker
-from sqlalchemy.util import immutabledict
 
 from src.config import Config as _Config
 from src.logger import Logger
 
-ConnectionDataType = TypedDict("ConnectionDataType", {
-    "drivername": str,
-    "database": str,
-    "username": str,
-    "password": str,
-    "host": str,
-    "port": int,
-    "query": immutabledict[str, tuple[str, ...] | str]
-})
-
-_connection_data: ConnectionDataType = {
-    'drivername': 'postgresql+psycopg',
-    'database': _Config.DB_NAME,
-    'username': _Config.DB_USER,
-    'password': _Config.DB_PASSWORD,
-    'host': _Config.DB_HOST,
-    'port': int(_Config.DB_PORT),
-    'query': immutabledict({})
-}
-
-_database_url = URL(**_connection_data)
+_database_url = _Config.DATABASE_URL.replace("postgresql://", "postgresql+psycopg://")
 
 _NAME = "database"
 
