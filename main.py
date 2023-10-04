f0820d9da12a199cd90668d71a2cb8047a1b5cc3
Juan Gahona <gahonajuanjo@gmail.com>
2023-10-04 11:41:49 -0500

    :sparkles: Up version. - Change version `v0.1.0` -> `v0.5.0`. - Update api description.

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from src import api
 app = api.create_app(
     title="ChatbotAPI | DIGO",
     version=api.__VERSION__,
-    description="This is the API for the Chatbot project.",
+    description="This is the API for the Saragurosnet Chatbot",
 )
