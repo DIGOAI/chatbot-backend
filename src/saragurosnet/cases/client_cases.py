e0730daae682d255774303d07324185fcd47543e
Juan Gahona <gahonajuanjo@gmail.com>
2023-10-05 10:54:35 -0500

    :sparkles: Create client if not exists. - Now the `GetClientByPhone` use case create a new client if this not exists in DB.

--- src/saragurosnet/cases/client_cases.py
+++ src/saragurosnet/cases/client_cases.py
@@ -1,5 +1,6 @@
 from src.api.cases.base_use_cases import UseCaseBase
 from src.api.repositories import ClientRepository
+from src.common.models import ClientInsert
 
 
 class GetClientByPhone(UseCaseBase):
@@ -7,5 +8,10 @@ class GetClientByPhone(UseCaseBase):
         with self._session() as session:
             client_repository = ClientRepository(session)
             client = client_repository.get_client_by_phone(client_phone)
+
+            if not client:
+                # Create a new client
+                new_client = ClientInsert.model_validate({"phone": client_phone})
+                client = client_repository.add_client(new_client)
 
         return client
