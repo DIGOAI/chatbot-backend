d1bbba8f412d33aae6c9a3d400ba0d40f7106f89
Juan Gahona <gahonajuanjo@gmail.com>
2023-10-06 15:21:15 -0500

    :sparkles: Create Job role model. - Created `JobRole` db model class. - Fixed imports.

--- src/api/db/models/__init__.py
+++ src/api/db/models/__init__.py
@@ -1,10 +1,12 @@
 from src.api.db.models.base import Base
 from src.api.db.models.client import Client
 from src.api.db.models.department import Department
+from src.api.db.models.job_role import JobRole
 
 __all__ = [
     "Base",
     "Department",
+    "JobRole",
 ]
 
 
