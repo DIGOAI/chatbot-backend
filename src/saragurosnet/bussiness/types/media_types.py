704847cb5931e1becd195bb4067de2093b4a2b72
Juan Gahona <gahonajuanjo@gmail.com>
2023-10-21 09:54:34 -0500

    :bug: Fix media url content. - Fixed media url for coverages.

--- src/saragurosnet/bussiness/types/media_types.py
+++ src/saragurosnet/bussiness/types/media_types.py
@@ -3,4 +3,4 @@ from typing import Final
 
 class MediaUrlType():
     PROMOTIONS: Final[str] = "https://i.ibb.co/Nn3ZDG7/saraguros-net-planes1.jpg"
-    COVERAGES: Final[str] = "https://i.ibb.co/9sQ8h5R/saraguros-net-cobertura.jpg"
+    COVERAGES: Final[str] = "https://i.ibb.co/LkZn7xZ/coverages.png"
