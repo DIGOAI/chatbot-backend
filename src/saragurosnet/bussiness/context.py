1083a9f074aed24ade48330f86322525f9389ba7
Juan Gahona <gahonajuanjo@gmail.com>
2023-10-26 11:08:35 -0500

    :sparkles: Update context class. - Added `conversation` attribute to `Context` class.

--- src/saragurosnet/bussiness/context.py
+++ src/saragurosnet/bussiness/context.py
@@ -1,14 +1,15 @@
-from src.common.models import Client
+from src.common.models import Client, Conversation
 from src.common.models import TwilioWebHook as Event
 
 
 class Context():
     """Context class to handle the context of the application."""
 
-    def __init__(self, event: Event, client: Client | None, last_state: str | None) -> None:
+    def __init__(self, event: Event, client: Client | None, last_state: str | None, conversation: Conversation) -> None:
         self.event_twilio: Event = event
         self.client: Client | None = client
+        self.conversation: Conversation = conversation
         self.last_state: str | None = last_state
 
     def __str__(self) -> str:
-        return f"Context(event_twilio={self.event_twilio}, client={self.client}, last_state={self.last_state})"
+        return f"Context(event_twilio={self.event_twilio}, client={self.client}, conversation={self.conversation}, last_state={self.last_state})"
