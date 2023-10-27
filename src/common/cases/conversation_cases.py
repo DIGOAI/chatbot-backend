76d4e6f2b1e793958349f1eed611d17f5d9029e0
Juan Gahona <gahonajuanjo@gmail.com>
2023-10-27 12:20:28 -0500

    :sparkles: Add many cases to conversations cases. - Added `get_conversations_resume` use case method. - Added `get_conversations_with_last_message` use case method. - Added `get_conversation_with_messages` use case method.

--- src/common/cases/conversation_cases.py
+++ src/common/cases/conversation_cases.py
@@ -1,4 +1,8 @@
 from datetime import datetime
+from typing import NamedTuple
+from uuid import UUID
+
+from sqlalchemy import select
 
 from src.common.cases.base_use_cases import UseCaseBase
 from src.common.models import (
@@ -6,9 +10,12 @@ from src.common.models import (
     Conversation,
     ConversationInsert,
     ConversationStatus,
+    ConversationWithData,
+    ConversationWithLastMessage,
 )
 from src.db.models import Conversation as ConversationModel
 from src.db.repositories import BaseRepository
+from src.db.repositories.base_repository import IdNotFoundError
 
 
 class ConversationUseCases(UseCaseBase):
@@ -46,3 +53,36 @@ class ConversationUseCases(UseCaseBase):
                 conversation.id, status=ConversationStatus.CLOSED, updated_at=conversation.updated_at)
 
         return conversation_updated
+
+    def get_conversations_resume(self):
+        ConversationResume = NamedTuple("ConversationResume", [("opened", int), ("closed", int)])
+
+        with self._session() as session:
+            opened = session.query(ConversationModel).filter(
+                ConversationModel.status == ConversationStatus.OPENED).count()
+            closed = session.query(ConversationModel).filter(
+                ConversationModel.status == ConversationStatus.CLOSED).count()
+
+        return ConversationResume(opened=opened, closed=closed)
+
+    def get_conversations_with_last_message(self, from_: int, to: int):
+        with self._session() as session:
+            stmt = select(ConversationModel).limit(to).offset(from_).join(ConversationModel.last_message)
+            conversations = session.scalars(stmt).all()
+
+            convers = conversations
+            conversations_with_last_message = [ConversationWithLastMessage.model_validate(c) for c in convers]
+
+        return conversations_with_last_message
+
+    def get_conversation_with_messages(self, conversation_id: UUID):
+        with self._session() as session:
+            stmt = select(ConversationModel).where(ConversationModel.id ==
+                                                   conversation_id).join(ConversationModel.messages)
+            conversations = session.scalar(stmt)
+
+            if conversations is None:
+                raise IdNotFoundError(conversation_id)
+            conversation_with_data = ConversationWithData.model_validate(conversations)
+
+        return conversation_with_data
