24d6066b18972a8b412bee27fdb056d2186bda58
Juan Gahona <gahonajuanjo@gmail.com>
2023-10-27 12:22:20 -0500

    :sparkles: Use correct use cases in routes. - Now the `ConversationUseCases` class is used in the `conversation_routes` file. - Now the `MessageUseCases` class is used in the `message_routes` file.

--- src/api/routes/message_routes.py
+++ src/api/routes/message_routes.py
@@ -1,30 +1,21 @@
-from typing import Annotated
+from fastapi import APIRouter, Depends, HTTPException
 
-from fastapi import APIRouter, Body, Depends, HTTPException
-
-from src.api.cases import get_last_messages, write_message
-from src.api.middlewares import APITokenAuth, JWTBearer
+from src.api.middlewares import JWTBearer
+from src.common.cases import MessageUseCases
 from src.common.models import GenericResponse, Message, MessageInsert, create_response
-from src.common.services.mock_data import messages as mock_messages
 
-router = APIRouter(prefix="/message", tags=["Message"])
+router = APIRouter(prefix="/message", tags=["Message"], dependencies=[Depends(JWTBearer())])
 
 
-@router.get("/", response_model=GenericResponse[list[Message]], dependencies=[Depends(JWTBearer())])
+@router.get("/", response_model=GenericResponse[list[Message]])
 def get_messages(limit: int = 10):
-    # return get_last_messages(limit)
+    messages = MessageUseCases().get_last_messages(limit)
 
-    messages = mock_messages.copy()
-    messages.sort(key=lambda m: m.created_at)
-
-    return create_response(messages[:limit], message=f"Last {limit} messages.")
+    return create_response(messages, message=f"Last {limit} messages.")
 
 
-@router.post("/", response_model=GenericResponse[Message], dependencies=[Depends(APITokenAuth())])
-def save_message(message: MessageInsert):
-    return write_message(message)
-
-
-@router.post("/messages/send", dependencies=[Depends(APITokenAuth())])
-def send_message(client_id: Annotated[str, Body(...)], message: Annotated[str, Body(...)]):
+@router.post("/send_message", response_model=GenericResponse[Message])
+def send_message(message: MessageInsert):
+    # message_is_created = MessageUseCases().add_new_message(message)
+    # return write_message(message)
     raise HTTPException(status_code=501, detail="Not implemented")
