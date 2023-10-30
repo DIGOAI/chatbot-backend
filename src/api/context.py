b1d07d182e9444e8939d05676c9fccbf5d6f252f
Juan Gahona <gahonajuanjo@gmail.com>
2023-10-30 16:50:01 -0500

    ⚡🚧 Simplified FastAPI context. - Removed `startup_event` async event function. - Removed `shutdown_event` async event function. - Simplified code.

--- src/api/context.py
+++ src/api/context.py
@@ -1,5 +1,4 @@
 from contextlib import asynccontextmanager
-from threading import Event
 
 import schedule
 from fastapi import FastAPI as _FastAPI
@@ -8,35 +7,19 @@ from src.common.logger import Logger
 from src.crons import run_schedules_continuously as _run_schedules_continuously
 from src.crons import service_close_to_expiration as _service_close_to_expiration
 
-_expiration_job: schedule.Job | None = None
-_schedules_thread: Event | None = None
 
-
-async def startup_event() -> None:
-    global _expiration_job, _schedules_thread
-
+@asynccontextmanager
+async def lifespan(app: _FastAPI):
+    # Execute startup procedure
     Logger.info("Starting events")
     # TODO: Change the day and hour to a value from the database
     _expiration_job = _service_close_to_expiration(30, "10:00")
     _schedules_thread = _run_schedules_continuously()
 
+    yield  # Run application
 
-async def shutdown_event() -> None:
-    global _expiration_job, _schedules_thread
-
-    if _schedules_thread is not None:
-        Logger.info("Stopping schedules thread")
-        _schedules_thread.set()
-
-    if _expiration_job is not None:
-        Logger.info("Stopping expiration job")
-        schedule.cancel_job(_expiration_job)
-
-
-@asynccontextmanager
-async def lifespan(app: _FastAPI):
-    await startup_event()
-
-    yield
-
-    await shutdown_event()
+    # Execute shutdown procedure
+    Logger.info("Stopping schedules thread")
+    _schedules_thread.set()
+    Logger.info("Stopping expiration job")
+    schedule.cancel_job(_expiration_job)
