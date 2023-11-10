70a8fb0d569207b22473d6c7f593845f3b1f7ef4
Juan Gahona <gahonajuanjo@gmail.com>
2023-11-10 17:12:52 -0500

    ✨ Add cron jobs update data from endpoints.

--- src/common/cases/options_cases.py
+++ src/common/cases/options_cases.py
@@ -3,6 +3,12 @@ from uuid import UUID
 
 from src.common.cases.base_use_cases import UseCaseBase
 from src.common.models import Options, OptionsUpdate
+from src.crons.data_reconciliation import (
+    data_reconciliation_process as _data_reconciliation_process,
+)
+from src.crons.service_expiration import (
+    service_close_to_expiration as _service_close_to_expiration,
+)
 from src.db.models import Options as OptionsModel
 from src.db.repositories import BaseRepository
 
@@ -26,5 +32,21 @@ class OptionsUseCases(UseCaseBase):
         with self._session() as session:
             options_repo = BaseRepository(OptionsModel, Options, session)
             options = options_repo.update(options_id, **data)
+
+        # Stop jobs
+        from src.crons import ScheduleManager
+        ScheduleManager.stop_schedules_continuously()
+
+        # Set new jobs updated
+        ScheduleManager.clear_jobs()
+        ScheduleManager.add_job(_service_close_to_expiration,  # type: ignore
+                                day=options.cutting_day,
+                                hour=options.cutting_hour.strftime("%H:%M"))
+        ScheduleManager.add_job(_data_reconciliation_process,  # type: ignore
+                                interval=options.data_reconciliation_interval,
+                                hour=options.data_reconciliation_hour.strftime("%H:%M"))
+
+        # Start jobs
+        ScheduleManager.run_schedules_continuously()
 
         return options
