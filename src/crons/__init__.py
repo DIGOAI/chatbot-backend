4f50469fc38b6edb6479a8ee52674e2bbb55cadc
Juan Gahona <gahonajuanjo@gmail.com>
2023-11-10 12:14:38 -0500

    ♻️ refactor ScheduleManager.

--- src/crons/__init__.py
+++ src/crons/__init__.py
@@ -1,7 +1,16 @@
-from src.crons.schedule_thread import run_schedules_continuously
-from src.crons.service_expiration import service_close_to_expiration
+from src.crons.data_reconciliation import (
+    data_reconciliation_process as _data_reconciliation_process,
+)
+from src.crons.schedule_thread import ScheduleManager as _ScheduleManager
+from src.crons.service_expiration import (
+    service_close_to_expiration as _service_close_to_expiration,
+)
+
+ScheduleManager = _ScheduleManager(interval=5)
+
+ScheduleManager.add_job(_service_close_to_expiration, day=10, hour="11:38")  # type: ignore
+ScheduleManager.add_job(_data_reconciliation_process, interval=1, hour="11:39")  # type: ignore
 
 __all__ = [
-    "run_schedules_continuously",
-    "service_close_to_expiration",
+    "ScheduleManager",
 ]
--- src/crons/schedule_thread.py
+++ src/crons/schedule_thread.py
@@ -1,81 +1,87 @@
+import asyncio
 import threading
-import time
+from typing import Any, NamedTuple, Protocol, Self
 
 import schedule
 
 
-class ScheduleThread(threading.Thread):
-    """ Clase para ejecutar trabajos programados en segundo plano."""
-
-    def __init__(self, event: threading.Event, interval: int = 1) -> None:
-        """ Inicializa la clase ScheduleThread.
-
-        Attributes:
-        event (threading.Event): Evento que se puede establecer para detener la ejecución continua.
-        interval (int): Intervalo de tiempo en segundos para verificar si hay trabajos pendientes. El valor predeterminado es 1.
-        """
-        self._cease_continuous_run = event
-        self._interval = interval
-        super().__init__()
-
-    def run(self) -> None:
-        while not self._cease_continuous_run.is_set():
-            schedule.run_pending()
-            seconds_to_next_execution = schedule.idle_seconds()
-            time.sleep(seconds_to_next_execution or self._interval)
+class ScheduleFunction(Protocol):
+    def __call__(self, **kwargs: Any) -> schedule.Job: ...
 
 
-def run_schedules_continuously(interval: int = 1):
-    """ Ejecuta los trabajos programados continuamente en segundo plano.
-    Continuamente ejecuta, mientras se ejecutan trabajos pendientes en cada intervalo de tiempo transcurrido.
-
-    Tenga en cuenta que es un comportamiento intencional que run_schedules_continuously() no ejecute trabajos perdidos.
-    Por ejemplo, si ha registrado un trabajo que debe ejecutarse cada minuto y establece un intervalo de ejecución 
-    continua de una hora, su trabajo no se ejecutará 60 veces en cada intervalo, sino solo una vez.
-
-    Attributes:
-    interval (int): Intervalo de tiempo en segundos para verificar si hay trabajos pendientes. El valor predeterminado es 1.
-
-    Returns: threading. Evento que se puede establecer para detener la ejecución continua.
-    """
-
-    cease_continuous_run = threading.Event()
-
-    continuous_thread = ScheduleThread(cease_continuous_run)
-    continuous_thread.start()
-    return cease_continuous_run
+class JobData(NamedTuple):
+    job_func: ScheduleFunction
+    job_params: dict[str, Any]
 
 
-# class ScheduleManager:
-#     _instance: "ScheduleManager" | None = None
+class ScheduleManager:
+    _instance: Self | None = None
+    _cease_continuous_run: threading.Event
+    _interval: int
+    _continuous_thread: "ScheduleManager.ScheduleThread"
+    _jobs_for_run: list[JobData] = []
+    _jobs_running: list[schedule.Job] = []
 
-#     def __new__(cls) -> "ScheduleManager":
-#         if cls._instance is None:
-#             cls._instance = super(ScheduleManager, cls).__new__(cls)
-#             cls._instance._cease_continuous_run: threading.Event = threading.Event()
-#             cls._instance._interval: int = 1
-#             cls._instance._continuous_thread: "ScheduleManager.ScheduleThread" = cls._instance.ScheduleThread()
+    def __new__(cls, interval: int = 1) -> "ScheduleManager":
+        if cls._instance is None:
+            cls._instance = super(ScheduleManager, cls).__new__(cls)
+            cls._instance._cease_continuous_run = threading.Event()
+            cls._instance._interval = interval
+            cls._instance._continuous_thread = cls._instance.ScheduleThread()
 
-#         return cls._instance
+        return cls._instance
 
-#     def run_schedules_continuously(self, interval: int = 1) -> None:
-#         self._instance._interval = interval
-#         self._instance._continuous_thread.start()
+    def _init_jobs(self) -> None:
+        for job_func, job_params in self._jobs_for_run:
+            self._jobs_running.append(job_func(**job_params))
 
-#     def get_cease_continuous_run(self) -> threading.Event:
-#         return self._instance._cease_continuous_run
+    def _stop_jobs(self) -> None:
+        for job in self._jobs_running:
+            schedule.cancel_job(job)
 
-#     class ScheduleThread(threading.Thread):
-#         def run(self) -> None:
-#             while not ScheduleManager._instance._cease_continuous_run.is_set():
-#                 schedule.run_pending()
-#                 seconds_to_next_execution = schedule.idle_seconds()
-#                 time.sleep(seconds_to_next_execution or ScheduleManager._instance._interval)
+    def add_job(self, job_func: ScheduleFunction, **job_params: Any) -> None:
+        self._jobs_for_run.append(JobData(job_func, job_params))
 
+    def run_schedules_continuously(self) -> None:
+        if self._instance is None:
+            raise ValueError("The ScheduleManager is not initialized")
 
-# # Uso:
-# manager: ScheduleManager = ScheduleManager()
-# manager.run_schedules_continuously()
+        # Initialize jobs
+        self._instance._init_jobs()
 
-# # Para acceder al atributo cease_continuous_run desde cualquier parte del código:
-# cease_continuous_run: threading.Event = manager.get_cease_continuous_run()
+        # self._instance._interval = interval
+        self._instance._continuous_thread.start()
+
+    async def _close_schedules_continuously(self) -> None:
+        if self._instance is None:
+            raise ValueError("The ScheduleManager is not initialized")
+
+        # Stop jobs
+        self._instance._stop_jobs()
+
+        self._instance._cease_continuous_run.set()
+        self._instance._continuous_thread.join()
+
+    async def stop_schedules_continuously(self):
+        task = asyncio.create_task(self._close_schedules_continuously())
+        return task
+
+    class ScheduleThread(threading.Thread):
+        """ Class to run scheduled jobs in the background. """
+
+        def __init__(self) -> None:
+            """ Initializes the ScheduleThread class. """
+            super().__init__(name="ScheduleThread")
+
+        def run(self) -> None:
+            if ScheduleManager._instance is None:
+                raise ValueError("The ScheduleManager is not initialized")
+
+            while not ScheduleManager._instance._cease_continuous_run.is_set():
+                schedule.run_pending()
+                seconds_to_next_execution = schedule.idle_seconds()
+                # time.sleep(seconds_to_next_execution or ScheduleManager._instance._interval)
+                # time.sleep(ScheduleManager._instance._interval)
+                ScheduleManager._instance._cease_continuous_run.wait(
+                    seconds_to_next_execution or ScheduleManager._instance._interval
+                )
