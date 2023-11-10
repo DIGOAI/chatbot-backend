c073aaa7d4d693334e055a26a2c0117eb6554293
Juan Gahona <gahonajuanjo@gmail.com>
2023-11-10 12:34:07 -0500

    ♻️ Refactored schedule management in context.py.

--- src/api/context.py
+++ src/api/context.py
@@ -1,25 +1,22 @@
 from contextlib import asynccontextmanager
 
-import schedule
 from fastapi import FastAPI as _FastAPI
 
 from src.common.logger import Logger
-from src.crons import run_schedules_continuously as _run_schedules_continuously
-from src.crons import service_close_to_expiration as _service_close_to_expiration
+from src.crons import ScheduleManager as _schedule_manager
 
 
 @asynccontextmanager
 async def lifespan(app: _FastAPI):
     # Execute startup procedure
-    Logger.info("Starting events")
-    # TODO: Change the day and hour to a value from the database
-    _expiration_job = _service_close_to_expiration(30, "10:00")
-    _schedules_thread = _run_schedules_continuously()
+    Logger.info("Starting application startup procedures")
+    _schedule_manager.run_schedules_continuously()
+    Logger.info("Started schedules thread")
 
     yield  # Run application
 
     # Execute shutdown procedure
-    Logger.info("Stopping schedules thread")
-    _schedules_thread.set()
-    Logger.info("Stopping expiration job")
-    schedule.cancel_job(_expiration_job)
+    Logger.info("Cleanning application startup procedures")
+
+    await _schedule_manager.stop_schedules_continuously()
+    Logger.info("Stopped schedules thread")
