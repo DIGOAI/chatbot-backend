bbbd271085171dcd487c140309669096c172c344
Juan Gahona <gahonajuanjo@gmail.com>
2023-11-13 15:45:39 -0500

    ✨ Add email use cases.

--- src/common/cases/__init__.py
+++ src/common/cases/__init__.py
@@ -1,12 +1,14 @@
 from src.common.cases.base_use_cases import UseCaseBase
 from src.common.cases.client_cases import ClientUseCases
 from src.common.cases.conversation_cases import ConversationUseCases
+from src.common.cases.email_cases import EmailUseCases
 from src.common.cases.message_cases import MessageUseCases
 from src.common.cases.options_cases import OptionsUseCases
 
 __all__ = [
     "ClientUseCases",
     "ConversationUseCases",
+    "EmailUseCases",
     "MessageUseCases",
     "OptionsUseCases",
     "UseCaseBase",
