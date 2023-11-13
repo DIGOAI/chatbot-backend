9edf2990cd4109f6cd73c8624e71270251c801b4
Juan Gahona <gahonajuanjo@gmail.com>
2023-11-13 11:17:19 -0500

    ♻️ Refactor ticket use cases. - Now use `UUID`. - Now use `Ticket` model.

--- src/api/cases/ticket_use_cases.py
+++ src/api/cases/ticket_use_cases.py
@@ -1,66 +1,36 @@
+from uuid import UUID
+
 from src.common.cases import UseCaseBase
-from src.common.cases.conversation_cases import ConversationUseCases
-from src.common.models import create_response
-
-#
-from src.common.models.ticket import Ticket as TicketPYModel
-from src.common.models.ticket import TicketStatus
-
-# common
-from src.common.types import idType
-
-# models
-from src.db.models.ticket import Ticket as TicketDBModel
-
-# db
+from src.common.models.ticket import Ticket, TicketStatus
+from src.db.models.ticket import Ticket as TicketModel
 from src.db.repositories import BaseRepository
 
 
 class TicketUseCase(UseCaseBase):
-    def list(self, limit: int = 30, offset: int = 0):
+    def get_tickets(self, limit: int = 30, offset: int = 0):
         with self._session() as session:
-            repository = BaseRepository(TicketDBModel, TicketPYModel, session)
+            repository = BaseRepository(TicketModel, Ticket, session)
             items = repository.list(offset, limit)
-        return create_response(data=items)
 
-    def get(self, item_id: idType):
+        return items
+
+    def get_ticket(self, item_id: UUID):
         with self._session() as session:
-            repository = BaseRepository(TicketDBModel, TicketPYModel, session)
+            repository = BaseRepository(TicketModel, Ticket, session)
             item = repository.get(item_id)
+
         return item
 
-    def add(self, data: dict):
+    def attend_ticket(self, item_id: UUID):
         with self._session() as session:
-            repository = BaseRepository(TicketDBModel, TicketPYModel, session)
-            item = repository.add(data, return_=True)
-        return item
+            ticket_repo = BaseRepository(TicketModel, Ticket, session)
+            result = ticket_repo.update(item_id, status=TicketStatus.ATTENDING)
 
-    def update(self, item_id: idType, data: dict):
-        with self._session() as session:
-            repository = BaseRepository(TicketDBModel, TicketPYModel, session)
-            item = repository.update(item_id, **data)
-        return item
-
-    def delete(self, item_id: idType):
-        with self._session() as session:
-            repository = BaseRepository(TicketDBModel, TicketPYModel, session)
-            result = repository.delete(item_id)
         return result
 
-    def open_ticket(self, item_id: idType):
-        data = {
-            "status": TicketStatus.ATTENDING
-        }
+    def close_ticket(self, item_id: UUID):
         with self._session() as session:
-            repository = BaseRepository(TicketDBModel, TicketPYModel, session)
-            result = self.update(item_id, data)
-        return result
+            ticket_repo = BaseRepository(TicketModel, Ticket, session)
+            result = ticket_repo.update(item_id, status=TicketStatus.CLOSED)
 
-    def close_ticket(self, item_id: idType):
-        data = {
-            "status": TicketStatus.CLOSED
-        }
-        with self._session() as session:
-            repository = BaseRepository(TicketDBModel, TicketPYModel, session)
-            result = self.update(item_id, data)
         return result
