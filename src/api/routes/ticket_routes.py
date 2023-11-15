7f48db0c9d29b33db0c804c2544e56b40575f2fb
Juan Gahona <gahonajuanjo@gmail.com>
2023-11-15 16:19:27 -0500

    ✨ Update ticket routes to include client info.

--- src/api/routes/ticket_routes.py
+++ src/api/routes/ticket_routes.py
@@ -5,7 +5,7 @@ from fastapi import APIRouter, Depends
 from src.api.cases.ticket_use_cases import TicketUseCase
 from src.api.middlewares.jwt_bearer import JWTBearer, Role
 from src.common.models import GenericResponse, create_response
-from src.common.models.ticket import Ticket
+from src.common.models.ticket import TicketWithClient
 
 _worker_dependency = Depends(JWTBearer(Role.WORKER))
 
@@ -14,15 +14,15 @@ router = APIRouter(prefix="/tickets", tags=["Tickets"], dependencies=[_worker_de
 controller = TicketUseCase()
 
 
-@router.get("/", response_model=GenericResponse[list[Ticket]])
+@router.get("/", response_model=GenericResponse[list[TicketWithClient]])
 def get_tickets(limit: int = 10, offset: int = 0):
-    result = controller.get_tickets(limit, offset)
+    result = controller.get_tickets_with_client(limit, offset)
     return create_response(result, "Tickets found")
 
 
-@router.get("/{ticket_id}", response_model=GenericResponse[Ticket])
+@router.get("/{ticket_id}", response_model=GenericResponse[TicketWithClient])
 def get_ticket(ticket_id: UUID):
-    result = controller.get_ticket(ticket_id)
+    result = controller.get_ticket_with_client(ticket_id)
     return create_response(result, "Ticket found")
 
 
