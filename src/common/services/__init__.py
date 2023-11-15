47a2664f04ddf7ce295cca68e1f01374cbbe65ee
Juan Gahona <gahonajuanjo@gmail.com>
2023-11-15 16:51:18 -0500

    🗑️ Remove unnused `BackendService`.

--- src/common/services/__init__.py
+++ src/common/services/__init__.py
@@ -1,4 +1,3 @@
-from src.common.services.backend_service import BackendService
 from src.common.services.chatgpt import ChatGPTService
 from src.common.services.ocr_lambda import OCRLambdaService
 from src.common.services.saragurosnet import SaragurosNetService
@@ -6,7 +5,6 @@ from src.common.services.smtp_service import SMTPService
 from src.common.services.twilio import TwilioService
 
 __all__ = [
-    "BackendService",
     "ChatGPTService",
     "OCRLambdaService",
     "SaragurosNetService",
