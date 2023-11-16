cd74a5f0f71bb56baa14a6878ea857a84e58cd0b
Juan Gahona <gahonajuanjo@gmail.com>
2023-11-16 12:00:16 -0500

    📝 Improvements in `DecisionsTree` class docs.

--- src/chatbot/decisions_tree/decisions_tree.py
+++ src/chatbot/decisions_tree/decisions_tree.py
@@ -19,6 +19,7 @@ class DecisionsTree(Generic[T]):
 
     Parameters:
     context (T): The context to use in the actions
+    start_id (str): The id of the action to start
     """
 
     def __init__(self, context: Optional[T] = None, start_id: str = "0.1") -> None:
@@ -36,14 +37,34 @@ class DecisionsTree(Generic[T]):
         self.__context = context
 
     def add_preaction(self):
+        """Add a preaction to the tree.
+
+        The preaction is a function that will be executed before the actions.
+
+        Returns:
+        Callable[[PreActionFunction[T]], None]: The decorator to add the preaction
+        """
+
         def inner_decorator(func: PreActionFunction[T]):
             @wraps(func)
             def wrapper():
                 self._preactions.append(func)
+
             return wrapper()
         return inner_decorator
 
     def add_action(self, id: str, condition: Callable[[T], bool], end: bool = True, next: str | list[str] = []):
+        """Add an action to the tree.
+
+        The action is a function that will be executed if the condition is true.
+
+        Attributes:
+        id (str): The id of the action
+        condition (Callable[[T], bool]): The condition to execute the action
+        end (bool): If the action is the last one
+        next (str | list[str]): The next action to execute
+        """
+
         def inner_decorator(func: ActionFunction[T]):
             @wraps(func)
             def wrapper():
@@ -53,6 +74,15 @@ class DecisionsTree(Generic[T]):
         return inner_decorator
 
     def __call__(self, next_actions: str | list[str] | None) -> str | list[str] | None:
+        """Build and execute the actions.
+
+        When the actions are executed, the preactions are executed before the actions.
+        The actions are executed until the end is reached.
+
+        Attributes:
+        next_actions (str | list[str] | None): The next actions to execute
+        """
+
         Logger.info(f"Building and executing actions")
 
         ended = False
