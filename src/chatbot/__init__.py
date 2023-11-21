66152266d9c74c09ebcd295a77514a3d7965ffae
Juan Gahona <gahonajuanjo@gmail.com>
2023-11-21 18:33:41 -0500

    ✨ Add `get_email` function.

--- src/chatbot/__init__.py
+++ src/chatbot/__init__.py
@@ -1,5 +1,10 @@
 from src.chatbot.decisions_tree import Action, ActionGroup, DecisionsTree
-from src.chatbot.utils import format_fullname, get_ci_or_ruc, get_phone_and_service
+from src.chatbot.utils import (
+    format_fullname,
+    get_ci_or_ruc,
+    get_email,
+    get_phone_and_service,
+)
 from src.chatbot.version import __VERSION__
 
 __all__ = [
@@ -9,5 +14,6 @@ __all__ = [
     "DecisionsTree",
     "format_fullname",
     "get_ci_or_ruc",
+    "get_email",
     "get_phone_and_service",
 ]
