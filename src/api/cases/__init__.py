f646669fa7de85bd4f94bae22edf167c71ea84fb
Juan Gahona <gahonajuanjo@gmail.com>
2023-11-22 11:14:48 -0500

    🗑️ Remove unused message cases and repository.

--- src/api/cases/__init__.py
+++ src/api/cases/__init__.py
@@ -1,11 +1,8 @@
 from src.api.cases.auth_use_cases import LoginUser, RegisterNewUser
-from src.api.cases.message_use_cases import get_last_messages, write_message
 from src.api.cases.template_use_cases import MassiveTemplateUseCases
 
 __all__ = [
-    "get_last_messages",
     "LoginUser",
     "MassiveTemplateUseCases",
     "RegisterNewUser",
-    "write_message",
 ]
