aeeeb6d9073dfc8dc616a948235f4432af8b8224
Juan Gahona <gahonajuanjo@gmail.com>
2023-11-23 16:40:02 -0500

    ♻️🎨🚚 Refactor logger. - Converted `Logger` class to singleton. - Updated the `__new__` initialization method. - Added log level configuration.

--- src/common/logger/logger.py
+++ src/common/logger/logger.py
@@ -1,4 +1,16 @@
+from typing import Final, Literal
+
 from src.common.logger.types import AlertType
+
+_LogLevel = Literal["ERROR", "WARNING", "ALERT", "INFO", "DEBUG"]
+
+_HIERARCHY_LOG_LEVEL: Final[dict[_LogLevel, list[AlertType]]] = {
+    "ERROR": [AlertType.ERROR],
+    "WARNING": [AlertType.ERROR, AlertType.WARNING],
+    "ALERT": [AlertType.ERROR, AlertType.WARNING, AlertType.ALERT],
+    "INFO": [AlertType.ERROR, AlertType.WARNING, AlertType.ALERT, AlertType.INFO],
+    "DEBUG": [AlertType.ERROR, AlertType.WARNING, AlertType.ALERT, AlertType.INFO, AlertType.DEBUG],
+}
 
 
 class Logger:
@@ -8,18 +20,31 @@ class Logger:
 
     _func_names_to_ignore = ["<module>", "__call__", "__new__"]
     _module_char_length = 40
+    _log_level: _LogLevel = "INFO"
 
-    def __new__(cls, func_names_to_ignore: list[str] | None = None, module_char_length: int | None = None):
+    def __new__(cls, log_level: _LogLevel = "INFO", func_names_to_ignore: list[str] = [], module_char_length: int = 40):
         if cls._instance is None:
             cls._instance = super().__new__(cls)
+
+            cls._instance._set_log_level(log_level)
 
             if func_names_to_ignore:
                 cls._instance._add_func_names_to_ignore(func_names_to_ignore)
 
-            if module_char_length:
+            if module_char_length > 0:
                 cls._instance._set_module_char_length(module_char_length)
 
         return cls._instance
+
+    @staticmethod
+    def _set_log_level(log_level: _LogLevel):
+        """Set the log level.
+
+        Parameters:
+        log_level (str): The log level
+        """
+
+        Logger._log_level = log_level
 
     @staticmethod
     def _set_module_char_length(module_char_length: int):
@@ -105,6 +130,9 @@ class Logger:
         msg (str): The message to log
         """
 
+        if alert_type not in _HIERARCHY_LOG_LEVEL[Logger._log_level]:
+            return
+
         if not caller_name:
             import inspect
             caller_frame = inspect.stack()[2]
@@ -116,5 +144,7 @@ class Logger:
                 caller_module_name = f"{caller_module_name.split('.')[-1]}.{caller_function_name}"
         else:
             caller_module_name = caller_name
+
+        # TODO: If alert_type is ERROR, WARNING or ALERT, save the log in a DB
 
         print(f"[{alert_type:^5}][{caller_module_name:^{Logger._module_char_length}}]: {msg}")
--- src/common/logger/types.py
+++ src/common/logger/types.py
@@ -4,10 +4,10 @@ from enum import Enum
 class AlertType(str, Enum):
     """Enum for the alert types."""
 
-    INFO = "INF"
     ERROR = "ERR"
     WARNING = "WAR"
     ALERT = "ALE"
+    INFO = "INF"
     DEBUG = "DEB"
 
     def __str__(self) -> str:
