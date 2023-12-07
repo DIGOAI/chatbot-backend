bd265f08c32244646e3c5d9fa50306de73391263
Juan Gahona <gahonajuanjo@gmail.com>
2023-12-07 18:36:14 -0500

    ♻️ Refactor router module. - Added socket event handlers in new file. - Moved sio events in top of module. - Removed unused code.

---
+++ src/socket/events/__init__.py
@@ -0,0 +1,3 @@
+from src.socket.events import events as sio_events
+
+__all__ = ["sio_events"]
--- src/socket/routers/__init__.py
+++
@@ -1,7 +0,0 @@
-from socketio import ASGIApp  # type: ignore
-
-from src.socket.routers.chat import router as router_chat
-
-
-def router(sio: ASGIApp):
-    router_chat(sio)
